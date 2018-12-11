116e93905abaee2b55b589d6949b623aaafd5842
javascript5 <javascript5@hotmail.com>
2018-12-11 12:58:33 +0700

    [Debug] Friend Fragment and Friend Adapter :bug:

--- FinalExam/app/src/main/java/a59070108/kmitl/ac/th/mobilefinal/Friend/FriendFragment.java
+++ FinalExam/app/src/main/java/a59070108/kmitl/ac/th/mobilefinal/Friend/FriendFragment.java
@@ -93,6 +93,6 @@ public class FriendFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_friend, container, false);
     }
 }
